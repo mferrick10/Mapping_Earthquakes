8d17e3f00bbce922dd40eb13851adf77aeb3f210
mferrick10 <mferrick11@gmail.com>
2020-04-17 19:28:09 -0400

    Code added to incorporate polylines into map

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -9,27 +9,23 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
+// Coordinates for each point to be used in the line.
+let line = [
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
 
-// Get data from cities.js
+// Create a polyline using the line coordinates and color the line
 
-let cityData = cities;
-
-// Looping through array of cities and creates map markers
-
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000,
-    color: 'orange',
-    weight: 4
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
-
+L.polyline(line, {
+  color: "yellow",
+  dashArray: '10, 10'
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
 	accessToken: API_KEY
